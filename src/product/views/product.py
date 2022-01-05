63ae53388697e97e1219a799b2b307823a8f7082
nipulkm <nipulkm@gmail.com>
2022-01-06 03:06:05 +0600

    Product list display implemented

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Variant, Product, ProductVariant, ProductVariantPrice
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +11,19 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+class ProductView(generic.TemplateView):
+    template_name='products/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductView, self).get_context_data(**kwargs)
+        products = Product.objects.all()
+        context['products'] = list(products)
+        variants = Variant.objects.all()
+        context['variants'] = list(variants)
+        product_variants = ProductVariant.objects.all()
+        context['product_variants'] = list(product_variants)
+        product_variant_prices = ProductVariantPrice.objects.all()
+        context['product_variant_prices'] = list(product_variant_prices)
         return context
