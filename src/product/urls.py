7019df8c292acf10daf5b0282c098f1bbd1f3fc2
nipulkm <nipulkm@gmail.com>
2022-01-07 00:32:30 +0600

    Filtered search result implemented

--- src/product/urls.py
+++ src/product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from product.views.product import CreateProductView, ProductView
+from product.views.product import CreateProductView, ProductView, ProductFilterView
 from product.views.variant import VariantView, VariantCreateView, VariantEditView
 
 app_name = "product"
@@ -16,4 +16,6 @@ urlpatterns = [
     path('create/', CreateProductView.as_view(), name='create.product'),
     path('list/page<int:page_number>/', ProductView.as_view(), name='list.product'),
     path('list/', ProductView.as_view(), name='list.product'),
+    path('list/filter/', ProductFilterView.as_view(), name='filter.product'),
+    path('list/filter/<str:title>/<int:price_from>/<int:price_to>/<str:date>/<int:page_number>/', ProductFilterView.as_view(), name='filter.product'),
 ]
--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,5 +1,7 @@
 import math
 
+from django.shortcuts import render
+
 from django.views import generic
 
 from product.models import Variant, Product, ProductVariant, ProductVariantPrice
@@ -40,5 +42,51 @@ class ProductView(generic.TemplateView):
             page_list.append(num)
         context['pagination'] = page_list
         context['previous_page'] = page_number - 1
-        context['next_page'] = page_number + 1
+        context['next_page'] = page_number + 1 if page_number + 1 <= pagination else 0
+        context['pagination_type'] = 'all_product_view'
+        return context
+
+class ProductFilterView(generic.TemplateView):
+    template_name='products/list.html'
+
+    def get_context_data(self, title='', price_from=0, price_to=0, date='', page_number=0, *args, **kwargs):
+        context = super(ProductFilterView, self).get_context_data(*args, **kwargs)
+        if page_number == 0:
+            title = self.request.GET['title']
+            price_from = self.request.GET['price_from']
+            price_to = self.request.GET['price_to']
+            date = self.request.GET['date']
+            page_number = 1
+        context['title'] = title
+        context['price_from'] = price_from
+        context['price_to'] = price_to
+        context['date'] = date
+        starting_page = (page_number - 1) * 2
+        end_page = page_number * 2
+        
+        variants = Variant.objects.all()
+        context['variants'] = list(variants)
+        product_variants = ProductVariant.objects.all()
+        context['product_variants'] = list(product_variants)
+        
+        products = Product.objects.filter(title=title)[starting_page:end_page]
+        context['products'] = list(products)
+        product_variant_prices = ProductVariantPrice.objects.filter(product__title=title, \
+            price__gte=price_from, price__lte=price_to)
+        context['product_variant_prices'] = list(product_variant_prices)
+
+        total_product = Product.objects.filter(title=title).count()
+        context['total_product'] = total_product
+        context['product_from'] = page_number * 2 -1
+        context['product_to'] = total_product if page_number * 2 > total_product else page_number * 2
+        pagination = math.ceil(total_product / 2)
+        page_list = []
+        for num in range(1, pagination+1):
+            page_list.append(num)
+        context['pagination'] = page_list
+        context['previous_page'] = page_number - 1
+        context['next_page'] = page_number + 1 if page_number + 1 <= pagination else 0
+        context['pagination_type'] = 'filtered_product_view'
+
+        print(context)
         return context
